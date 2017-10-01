f28cb038c4439fe537a78cca61ab9057cee895e2
Julian Gruber <julian@juliangruber.com>
2017-10-01 16:41:32 +0200

    print npm install output

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const { promisify } = require('util')
 const json = promisify(require('body/json'))
 const tmpdir = require('os').tmpdir()
 const mkdir = promisify(require('fs').mkdir)
-const spawn = require('child_process').spawn
-const exec = promisify(require('child_process').exec)
+const cp = require('child_process')
 const unlink = promisify(require('fs').unlink)
 const writeFile = promisify(require('fs').writeFile)
 const pipe = require('promisepipe')
+
+const spawnAsync = (...args) =>
+  new Promise((resolve, reject) => {
+    const ps = cp.spawn(...args)
+    ps.on('error', reject)
+    ps.on('exit', resolve)
+  })
 
 module.exports = async (req, res) => {
   const pkg = await json(req, res)
@@ -17,7 +23,7 @@ module.exports = async (req, res) => {
     .slice(2)}`
   await mkdir(cwd)
   await writeFile(`${cwd}/package.json`, JSON.stringify(pkg))
-  await exec('npm install', { cwd })
+  await spawnAsync('npm', ['install'], { cwd, stdio: 'inherit' })
   await unlink(`${cwd}/package.json`)
-  await pipe(spawn('tar', ['-zc', '.'], { cwd }).stdout, res)
+  await pipe(cp.spawn('tar', ['-zc', '.'], { cwd }).stdout, res)
 }
